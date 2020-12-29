d5bc2d91b10fe667ae911c6c48c6dd1bef88ba5d
akhalizev <ahalizev@gmail.com>
2020-12-29 12:45:57 -0600

    deploy

---
+++ static/pwa/precache-manifest.27a0ac1a9dc0e1b671e9551a2320ccc8.js
@@ -0,0 +1,36 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba124936d6d159307064d98707a9c35",
+    "url": "/index.html"
+  },
+  {
+    "url": "/static/icons/icon_128x128.ee2678ee2de2d0955345a009430dae12.png"
+  },
+  {
+    "url": "/static/icons/icon_192x192.2e7a9f09eb4ce3a26b0d9ed7ea93e406.png"
+  },
+  {
+    "url": "/static/icons/icon_256x256.bfba6030220a6f9a2272a42de53917ea.png"
+  },
+  {
+    "url": "/static/icons/icon_384x384.60eff8c0760fa92894606fdc1fae7cf0.png"
+  },
+  {
+    "url": "/static/icons/icon_512x512.6475f27d549c0b523266292a0375d04e.png"
+  },
+  {
+    "url": "/static/icons/icon_96x96.111d9abf045a3e83af60c74a5da91e8c.png"
+  },
+  {
+    "url": "/static/images/e18977fd23c4850b464c07ab3d5e9f32.png"
+  },
+  {
+    "url": "/static/main.078b5382dfc117000383.css"
+  },
+  {
+    "url": "/static/main.078b5382dfc117000383.js"
+  },
+  {
+    "url": "/static/manifest.f2462f2573d3da22f0f247735ef56c36.json"
+  }
+]);
